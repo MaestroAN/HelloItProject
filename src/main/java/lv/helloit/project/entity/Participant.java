daafccb681ef8a1350562088747fdfa6eb3cc0a5
Antony <maestro113an@gmail.com>
2019-01-14 21:59:41 +0200

    added unique key to code column in participant table

--- src/main/java/lv/helloit/project/entity/Participant.java
+++ src/main/java/lv/helloit/project/entity/Participant.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "participant")
+@Table(name = "participant", uniqueConstraints = {@UniqueConstraint(columnNames = "code")})
 public class Participant {
 
     @Id
